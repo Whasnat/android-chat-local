d15183573eb88fad412a0ca31d051653f0bb8a93
w.hasnat94@gmail.com <git94w.hasnat>
2020-02-13 01:27:24 -0800

    first commit

---
+++ app/src/main/java/com/example/myapplication/HomeActivity.java
@@ -0,0 +1,43 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.google.firebase.auth.FirebaseAuth;
+
+public class HomeActivity extends AppCompatActivity {
+
+    private Button ulogout, contacts_btn;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+        ulogout = findViewById(R.id.log_out_btn);
+        contacts_btn = findViewById(R.id.contacts_btn);
+
+
+        contacts_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(),FindContactsActivity.class);
+                startActivity(intent);
+            }
+        });
+        ulogout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                FirebaseAuth.getInstance().signOut();
+                Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                intent.addFlags(intent.FLAG_ACTIVITY_CLEAR_TOP);
+                startActivity(intent);
+                finish();
+                return;
+            }
+        });
+    }
+}
---
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -0,0 +1,137 @@
+package com.example.myapplication;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.FirebaseException;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.PhoneAuthCredential;
+import com.google.firebase.auth.PhoneAuthProvider;
+
+import java.util.concurrent.TimeUnit;
+
+public class MainActivity extends AppCompatActivity {
+    private static final String TAG = "MainActivity";
+    private EditText uphone, vcode;
+    private PhoneAuthProvider.OnVerificationStateChangedCallbacks ucallbacks;
+    private Button usend;
+    String verificationID;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        FirebaseApp.initializeApp(this);
+        vcode = findViewById(R.id.code);
+        //1.Checks if the user is logged In
+        userActive();
+        //
+
+        //2.Enter Phone Number
+        uphone = findViewById(R.id.phone_no);
+        usend = findViewById(R.id.send_btn);
+        //3.On Button Press
+        usend.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //If automatic Verification fails
+                if (verificationID != null)
+                    verifyPhoneNumberWithCode(verificationID, vcode.getText().toString());
+
+                else
+                //4.Authenticate By sending OTP to Phone and verifying User Login
+                Phoneauth();
+            }
+        });
+
+
+        //6.Use the Callback from "5" check PASS/FAIL
+        ucallbacks = new PhoneAuthProvider.OnVerificationStateChangedCallbacks() {
+
+            //When the Verification Code is sent
+            @Override
+            public void onCodeSent(String s, PhoneAuthProvider.ForceResendingToken forceResendingToken) {
+                super.onCodeSent(s, forceResendingToken);
+                usend.setText("Verify");
+                Log.d(TAG, "onCodeSent: Code Sent: " + s);
+                verificationID = s;
+            }
+
+            @Override
+            //IF PASS
+            public void onVerificationCompleted(PhoneAuthCredential phoneAuthCredential) {
+                //call to Authenticate with PhoneCredentials
+                signInWithCredentials(phoneAuthCredential);
+            }
+
+            @Override
+            //IF FAIL
+            public void onVerificationFailed(FirebaseException e) {
+
+            }
+
+
+        };
+    }
+
+
+    //Verify the Code With the Phone Verification Code
+    private void verifyPhoneNumberWithCode(String verificationId, String code) {
+        // [START verify_with_code]
+        PhoneAuthCredential credential = PhoneAuthProvider.getCredential(verificationId, code);
+        // [END verify_with_code]
+        signInWithCredentials(credential);
+    }
+
+
+    //7.SignIN with FirebaseAuth
+    private void signInWithCredentials(PhoneAuthCredential credential) {
+        FirebaseAuth.getInstance().signInWithCredential(credential).addOnCompleteListener(this,
+                new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        //IF PASS
+                        if (task.isSuccessful())
+                            //call to mark the current User as Active
+                            userActive();
+                    }
+                });
+    }
+
+
+    //8. make the current user as Active and Go to HOME PAGE
+    private void userActive() {
+        FirebaseUser currentUser = FirebaseAuth.getInstance().getCurrentUser();
+
+        //Check again if the user is active
+        if (currentUser != null) {
+            startActivity(new Intent(getApplicationContext(), HomeActivity.class));
+            //Finish the Login Activity so the Active user cant go back to login page without logging out
+            finish();
+            return;
+        }
+
+    }
+
+
+    //5.Send Otp and return the Callback from Phone Number Within Time Limit
+    private void Phoneauth() {
+        PhoneAuthProvider.getInstance().verifyPhoneNumber(
+                uphone.getText().toString(),
+                60,
+                TimeUnit.SECONDS,
+                this,
+                ucallbacks);
+    }
+}
